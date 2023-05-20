07b15e3bd18d6217bba97b4b4331b50d7df98874
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-05-21 02:01:20 +0600

    frontend: search bar

--- frontend/src/components/SearchBarComponent.vue
+++ frontend/src/components/SearchBarComponent.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="input-group input-group-lg">
-      <input type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Type your word here...">
+    <div class="mb-3">
+        <div class="row">
+            <div class="col-6">
+                <label for="exampleInputEmail1" class="form-label">Base</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>English</option>
+                    <option value="1">One</option>
+                </select>
+            </div>
+            <div class="col-6">
+                <label for="exampleInputEmail1" class="form-label">Secondary</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Bengali</option>
+                    <option value="1">One</option>
+                </select>
+            </div>
+        </div>
+        <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Type your word here...">
+        <div id="emailHelp" class="form-text">When you will stop typing the result will be shown in below.</div>
     </div>
 </template>
 
